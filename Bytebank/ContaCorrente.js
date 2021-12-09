1ae4c5403065cc68a1a87566f6d468132310a9ee
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-12-08 22:15:58 -0300

    finalizando Bytebank em JS

--- Bytebank/ContaCorrente.js
+++ Bytebank/ContaCorrente.js
@@ -1,18 +1,52 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-    agencia;
-    _saldo = 0;
+  static numeroDeContas = 0;
+  agencia;
+  _cliente;
 
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
+  /**
+   * @param {Cliente} novoValor
+   */
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
     }
+  }
 
-    depositar(valor) {
-        if (valor <= 0) {
-            return;
-        }
-        this._saldo += valor;
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+  constructor(agencia, cliente) {
+    this.agencia = agencia;
+    this.cliente = cliente;
+    ContaCorrente.numeroDeContas += 1;
+  }
+
+  _saldo = 0;
+
+  sacar(valor) {
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return valor;
     }
+  }
+
+  depositar(valor) {
+    if (valor <= 0) {
+      return;
+    }
+    this._saldo += valor;
+  }
+
+  transferir(valor, conta) {
+    conta.cidade = "São Paulo";
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+  }
 }
--- Bytebank/index.js
+++ Bytebank/index.js
@@ -1,25 +1,25 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
+const cliente1 = new Cliente("Ricado", 44347865815);
 
-cliente1.nome = "Ricado";
-cliente1.cpf = 44347865815;
+const cliente2 = new Cliente("Alice", 88255645755);
 
-
-const cliente2 = new Cliente();
-
-cliente2.nome = "Alice";
-cliente2.cpf = 88255645755;
-
-
-const ContaCorrenteRicardo = new ContaCorrente();
-ContaCorrenteRicardo.agencia = 1001;
+const ContaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 
 ContaCorrenteRicardo.depositar(100);
 ContaCorrenteRicardo.depositar(150);
-ContaCorrenteRicardo.depositar(-50);
+ContaCorrenteRicardo.depositar(50);
+
+console.log("Contas abertas: ", ContaCorrente.numeroDeContas);
 
 ContaCorrenteRicardo.sacar(170);
 
 console.log(ContaCorrenteRicardo);
+
+const conta2 = new ContaCorrente(1002, cliente2);
+
+ContaCorrenteRicardo.transferir(100, conta2);
+console.log("");
+console.log(conta2);
+console.log("");
